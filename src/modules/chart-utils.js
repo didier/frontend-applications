e112267a6d2469a02124efc90dfde0aed75e808c
Didier Catz <didierlennon@gmail.com>
2020-11-24 22:12:18 +0100

    Add new getAxisValues function for axis properties

--- src/modules/chart-utils.js
+++ src/modules/chart-utils.js
@@ -87,3 +87,19 @@ export function render(
     .attr('width', (d) => xScale(d.hourlyCost))
     .attr('height', yScale.bandwidth())
 }
+
+/**
+ * Gets the properties of each axis
+ *
+ * @export
+ * @param {String} x the lookup property of the X-axis
+ * @param {String} y the lookup property of the Y-axis
+ * @returns {Array} the X and Y values
+ */
+export function getAxisValues(x, y) {
+  return ([
+    (data) => data[x],
+    (data) => data[y]
+  ])
+}
+
