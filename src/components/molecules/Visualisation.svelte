78876f6620bb6b6f0679bdb433ad418201ab757d
Didier Catz <didierlennon@gmail.com>
2020-11-24 22:11:24 +0100

    Render Barchart when data is loaded

--- src/components/molecules/Visualisation.svelte
+++ src/components/molecules/Visualisation.svelte
@@ -1,15 +1,12 @@
 <script>
-  import Bar from '../atoms/Bar.svelte'
   import cleanData from '/src/modules/data'
+  import Bar from '../atoms/Bar.svelte'
+
   const data = cleanData()
-  let width
 </script>
 
 {#await data}
   <h1>Loading...</h1>
 {:then data}
-  <!-- <h1>{JSON.stringify(data, null, 2)}</h1> -->
-  <div bind:clientWidth={width}>
-    <Bar {data} {width} />
-  </div>
+  <Bar {data} />
 {/await}
